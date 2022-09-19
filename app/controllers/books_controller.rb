43a47e4f5da2d08e4060ccdda6f20d01f8c4e63d
TannoDaichi <nekonindaichi15@gmail.com>
2022-09-19 07:48:20 +0000

    SAVE1_debug2-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,10 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def new
+    @book = Book.new
+  end
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
