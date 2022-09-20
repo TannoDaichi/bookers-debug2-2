afeb61b6739f18a51e76e7998bef2bde42044f76
TannoDaichi <nekonindaichi15@gmail.com>
2022-09-20 06:36:43 +0000

    SAVE2_debug2-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,13 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def index
     @books = Book.all
   end
 
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
@@ -45,6 +43,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
